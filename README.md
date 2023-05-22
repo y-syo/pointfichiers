f16b960411ce7f1141cb6ee0ad30f74f35c8310f
hobosyo <yosyomiyori@gmail.com>
2023-05-22 11:42:55 +0200

    vim

--- README.md
+++ README.md
@@ -1,36 +1,3 @@
 # Dotfiles
 
-kanagawa x bspwm my beloved
-
-![image](/images/full.jpg)
-
-# Installation
-
-### Dependencies :
-
-- [bspwm](https://github.com/baskerville/bspwm)
-
-- [sxhkd](https://github.com/baskerville/sxhkd)
-
-- [picom](https://github.com/yshui/picom)
-
-- [alacritty](https://github.com/alacritty/alacritty)
-
-- [eww](https://github.com/elkowar/eww)
-
-- [rofi](https://github.com/davatorium/rofi)
-
-- [zsh](https://github.com/zsh-users/zsh)
-
-
-### actual installation :
-
-just execute the install.sh and the dotfiles are now active, ez
-
-```
-$ ./install.sh
-```
-
-# others
-
-[wallpaper](https://keke-i.tumblr.com/post/666805015939301376)
+# (WIP)
--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-ln -s  -d "$DIR/.config/alacritty" ~/.config/alacritty
-ln -s -d "$DIR/.config/bspwm" ~/.config/bspwm
-ln -s -d "$DIR/.config/eww" ~/.config/eww
-ln -s -d "$DIR/.config/picom" ~/.config/picom
-ln -s -d "$DIR/.config/sxhkd" ~/.config/sxhkd
-ln -s -d  "$DIR/.config/rofi" ~/.config/rofi
-ln -s -d  "$DIR/.zshrc" ~/
+#ln -s  -d "$DIR/.config/alacritty" ~/.config/alacritty
+#ln -s -d "$DIR/.config/bspwm" ~/.config/bspwm
+#ln -s -d "$DIR/.config/eww" ~/.config/eww
+#ln -s -d "$DIR/.config/picom" ~/.config/picom
+#ln -s -d "$DIR/.config/sxhkd" ~/.config/sxhkd
+#ln -s -d  "$DIR/.config/rofi" ~/.config/rofi
+#ln -s -d  "$DIR/.zshrc" ~/

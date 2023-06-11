87e98233aaeec7cf244bb3670e96cdb93766a739
hobosyo <yosyomiyori@gmail.com>
2023-06-11 12:45:55 +0200

    install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-#ln -s  -d "$DIR/.config/alacritty" ~/.config/alacritty
-#ln -s -d "$DIR/.config/bspwm" ~/.config/bspwm
-#ln -s -d "$DIR/.config/eww" ~/.config/eww
-#ln -s -d "$DIR/.config/picom" ~/.config/picom
-#ln -s -d "$DIR/.config/sxhkd" ~/.config/sxhkd
-#ln -s -d  "$DIR/.config/rofi" ~/.config/rofi
-#ln -s -d  "$DIR/.zshrc" ~/
+ln -s -d "$DIR/.config/eww" "$XDG_CONFIG_DIR/eww"
+ln -s -d "$DIR/.config/hypr" "$XDG_CONFIG_DIR/hypr"
+ln -s -d "$DIR/.config/kitty" "$XDG_CONFIG_DIR/kitty"
+ln -s -d "$DIR/.config/tofi" "$XDG_CONFIG_DIR/tofi"
+ln -s -d "$DIR/.config/waybar" "$XDG_CONFIG_DIR/waybar"
+ln -s -d "$DIR/.config/gtk-3.0" "$XDG_CONFIG_DIR/gtk-3.0"
+ln -s -d "$DIR/.config/gtk-4.0" "$XDG_CONFIG_DIR/gtk-4.0"
+
+ln -s "$DIR/.gtkrc-2.0" "$HOME/.gtkrc-2.0"
+ln -s "$DIR/.zshrc" "$HOME/.zshrc"
+
